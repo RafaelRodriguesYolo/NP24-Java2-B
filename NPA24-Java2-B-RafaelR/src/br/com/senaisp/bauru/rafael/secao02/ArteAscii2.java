1c3280b261d84e05dec6f2bec56998bebd16fa32
RafaelRodriguesYolo <rafael_cloud44@hotmail.com>
2024-03-13 22:05:48 -0300

    Conteúdo da aula do dia 13-03-2024 / Arte Ascii

---
+++ NPA24-Java2-B-RafaelR/src/br/com/senaisp/bauru/rafael/secao02/ArteAscii2.java
@@ -0,0 +1,21 @@
+package br.com.senaisp.bauru.rafael.secao02;
+
+public class ArteAscii2 {
+
+	public static void main(String[] args) {
+		System.out.println("╔═════════════╗");
+		System.out.println("║ □    □    □ ║");
+		System.out.println("║ □    □    □ ║");
+		System.out.println("║ □    □    □ ║");
+		System.out.println("║ □    □    □ ║");
+		System.out.println("║ □    □    □ ║");
+		System.out.println("║ □    □    □ ║");
+		System.out.println("║ □    □    □ ║");
+		System.out.println("║ □    □    □ ║");
+		System.out.println("║ □    □    □ ║");
+		System.out.println("║ □    □    □ ║");
+		System.out.println("║ □  ┌───┐  □ ║");
+		System.out.println("║ □  │   │  □ ║");
+	}
+
+}
