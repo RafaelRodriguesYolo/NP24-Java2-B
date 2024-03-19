d2df06cb2e3a39e041be5aebf72e7ce5aa441ab7
RafaelRodriguesYolo <rafael_cloud44@hotmail.com>
2024-03-18 22:03:44 -0300

    Conteúdo da aula do dia 18-03-2024

---
+++ NPA24-Java2-B/src/br/com/senaisp/bauru/rafael/secao04/ConversaoTipos01.java
@@ -0,0 +1,15 @@
+package br.com.senaisp.bauru.rafael.secao04;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class ConversaoTipos01 {
+
+	public static void main(String[] args) {
+		int num1 = 55555;
+		int num2 = 66666;
+		long num3 = 1L * num1 * num2;
+		System.out.println(num3);
+		System.out.println(Integer.MAX_VALUE);
+	}
+
+}
